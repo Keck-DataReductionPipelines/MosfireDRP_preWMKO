196ba21b1ea622e43c36210f4e4a72f20b97f326
Robert Lasenby <rnl22@cam.ac.uk>
2009-07-29 22:52:11 +0100

    Separated out console interface for simulator

--- spectroscopicSimulator/mosfireSim.py
+++ spectroscopicSimulator/mosfireSim.py
@@ -2,8 +2,6 @@
 # Robert Lasenby 2009
 
 from simulator import *
-import sys
-import getopt
 import os
 
 mosfire = Instrument()
@@ -87,41 +85,3 @@ def calcCountImage (bandName):
 	computeSlitOverlaps (mosfire)
 	computeSlitGroups (mosfire)
 	return drawSlits (mosfire, band)
-
-def main ():
-	global detectorQEFile
-	opts, args = getopt.getopt (sys.argv[1:],"b:o:rst:pq")
-	outName = "output.fits"
-	bandName = "K"
-	exposureTime = 1
-	poissonNoise = False
-	useDetectorQE = False
-	for o,a in opts:
-		if o == "-b": 
-			bandName = a
-		elif o == "-o": 
-			outName = a
-		#elif o == "-s":
-			#mosfire.slitX
-		elif o == "-r":
-			mosfire.slitX = 0.6 * np.random.rand (mosfire.nBars) - 0.3
-		elif o == "-t":
-			exposureTime = float(a)
-		elif o == "-p":
-			poissonNoise = True
-		elif o == "-q":
-			useDetectorQE = True
-			if a != '':
-				detectorQEFile = a
-	im = exposureTime * calcCountImage(bandName)
-	if useDetectorQE:
-		qe0 = pyfits.getdata (detectorQEFile)
-		qe = np.clip (qe0, 0.0, 1.0)
-		im *= qe
-	if poissonNoise:
-		im = genPoissonVals (im)
-	#saveAsFits (im, outName)
-	saveAsFitsWithExtensions (mosfire, im, outName)
-
-if __name__ == "__main__":
-	main()
---
+++ spectroscopicSimulator/mosfireSimConsole.py
@@ -0,0 +1,42 @@
+# Simulation of MOSFIRE output, spectroscopic mode 
+# Robert Lasenby 2009
+
+from mosfireSim import *
+import sys
+import getopt
+
+def main ():
+	global detectorQEFile
+	opts, args = getopt.getopt (sys.argv[1:],"b:o:rt:pq")
+	outName = "output.fits"
+	bandName = "K"
+	exposureTime = 1
+	poissonNoise = False
+	useDetectorQE = False
+	for o,a in opts:
+		if o == "-b": 
+			bandName = a
+		elif o == "-o": 
+			outName = a
+		elif o == "-r":
+			mosfire.slitX = 0.6 * np.random.rand (mosfire.nBars) - 0.3
+		elif o == "-t":
+			exposureTime = float(a)
+		elif o == "-p":
+			poissonNoise = True
+		elif o == "-q":
+			useDetectorQE = True
+			if a != '':
+				detectorQEFile = a
+	im = exposureTime * calcCountImage(bandName)
+	if useDetectorQE:
+		qe0 = pyfits.getdata (detectorQEFile)
+		qe = np.clip (qe0, 0.0, 1.0)
+		im *= qe
+	if poissonNoise:
+		im = genPoissonVals (im)
+	#saveAsFits (im, outName)
+	saveAsFitsWithExtensions (mosfire, im, outName)
+
+if __name__ == "__main__":
+	main()
