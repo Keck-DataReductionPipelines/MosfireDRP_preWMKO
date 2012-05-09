5057e9db3de3303ef459c6d78bd905bb6f97c624
Nick Konidaris <nick.konidaris@gmail.com>
2012-05-08 21:27:18 -1000

    Created

---
+++ MOSFIRE/Filters.py
@@ -0,0 +1,17 @@
+
+
+
+'''
+    MOSFIRE Filter information
+
+    [YJHK]hpp : the half power points of filters in micron
+
+'''
+
+hpp = {
+        'Y': [0.972, 1.124],
+        'J': [1.153, 1.352],
+        'H': [1.466, 1.807],
+        'K': [1.921, 2.404]
+        }
+
