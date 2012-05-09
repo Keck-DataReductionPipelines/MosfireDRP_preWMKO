694283186bcd31904d8ede53e4d936809b74f54e
Nick Konidaris <nick.konidaris@gmail.com>
2012-05-08 21:27:40 -1000

    Minor changes

--- MOSFIRE/Options.py
+++ MOSFIRE/Options.py
@@ -17,8 +17,8 @@ __version__ = "19_04_2011"
 
 npix = 2048
 
-flat = {"outdir": "/scr2/mosfire/firstlight/",
-        "indir": "/scr2/mosfire/firstlight/",
+flat = {"outdir": "/scr2/mosfire/secondlight/",
+        "indir": "/scr2/mosfire/secondlight/",
         "version": 1, 
         "edge-order": 5, # Polynomial order for edge of slit 
         "edge-fit-width": 20,
@@ -28,7 +28,9 @@ flat = {"outdir": "/scr2/mosfire/firstlight/",
 
 wavelength = {"outdir": flat["outdir"],
         "indir": flat["indir"],
+        "datadir" : "/Users/npk/Dropbox/MOSFIRE/code/data",
         "version": 1,
-        "fractional-wavelength-search": 0.9988, # used in determining oned wavelength solutions
+        #"fractional-wavelength-search": 0.9988, # used in determining oned wavelength solutions
+        "fractional-wavelength-search": 0.99935, # used in determining oned wavelength solutions
 
 }
