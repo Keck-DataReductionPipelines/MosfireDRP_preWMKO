be41141ff2bb821d7a682d586f97d35cabe32a1c
Nick Konidaris <nick.konidaris@gmail.com>
2012-05-23 09:33:44 +0300

    For general purpose use

--- experiments/egs_abs.py
+++ experiments/egs_abs.py
@@ -22,46 +22,37 @@ maskname = 'egs_abs'
 band = 'J'
 
 
-if False:
+if True:
     flatlist = range(14,24)
     fs = []
     for flat in flatlist:
-        fn = os.path.join("/users/npk/desktop/7may2012/m120507_%4.4i.fits" % flat)
-        fs.append(fn)
+        fs.append("m120507_%4.4i.fits" % flat)
 
     options = Options.flat
     options["outdir"] = "/scr2/mosfire/secondlight/"
+    options["indir"] = "/users/npk/desktop/"
     Flats.handle_flats(fs, maskname, band,  options)
 
 
 options = Options.wavelength
 options["outdir"] = "/scr2/mosfire/secondlight/"
-options["indir"] = "/users/npk/desktop/7may2012/"
-path = os.path.join(options["outdir"], maskname)
-if not os.path.exists(path):
-    raise Exception("Output directory '%s' does not exist. This " 
-            "directory should exist." % path)
+options["indir"] = "/users/npk/desktop/"
 
 
 ind = options['indir']
 fs = ['m120507_0230.fits']
 np.set_printoptions(precision=2)
-if True:
+if False:
     for fname in fs:
-        fp = os.path.join(ind, fname)
-
-        mfits = IO.readmosfits(fp)
+        mfits = IO.readmosfits(fname, options)
         header, data, bs = mfits
 
-        Wavelength.plot_mask_solution_ds9(fname, maskname, options)
         #Wavelength.fit_lambda_interactively(mfits, fname, maskname, options)
-        Wavelength.fit_lambda(mfits, fname, maskname, options)
-        Wavelength.apply_lambda(mfits, fname, maskname, options)
-        Wavelength.plot_data_quality(maskname, fname, options)
-        Wavelength.plot_sky_spectra(maskname, fname, options)
-        Wavelength.plot_mask_fits(maskname, fname, options)
+        #Wavelength.fit_lambda(mfits, fname, maskname, options)
+        #Wavelength.apply_lambda_simple(mfits, fname, maskname, options)
+        #Wavelength.plot_mask_fits(maskname, fname, options)
 
-if True:
+if False:
 
     As = [ind + "m120507_%4.4i.fits" % i for i in range(229,249,2)]
     Bs = [ind + "m120507_%4.4i.fits" % i for i in range(230,249,2)]
