4b8c52ba1971f7c16e1d4ebb7d1cb9fd42650726
Nick Konidaris <nick.konidaris@gmail.com>
2012-06-02 16:43:01 -1000

    minor changes

--- experiments/egs_abs.py
+++ experiments/egs_abs.py
@@ -53,10 +53,13 @@ if False:
         #Wavelength.fit_lambda(mfits, fname, maskname, options)
         Wavelength.apply_lambda_simple(mfits, fname, maskname, options)
 
-if False:
+if True:
 
     As = ["m120507_%4.4i.fits" % i for i in range(229,249,2)]
     Bs = ["m120507_%4.4i.fits" % i for i in range(230,249,2)]
+
+    As.extend(["m120509_%4.4i.fits" % i for i in range(315,369,2)])
+    Bs.extend(["m120509_%4.4i.fits" % i for i in range(316,369,2)])
 
     Background.handle_background(As, Bs, lname, maskname,
             band, options)
